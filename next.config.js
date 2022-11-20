d2cb4288cd505e1cbf43579137901a1dbfe98ad9
Leif Arriens <leif.arriens@gmail.com>
2022-11-20 06:09:55 +0100

    add: search scrollbar & packages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['i.scdn.co'],
+  },
   experimental: {
     fontLoaders: [
       { loader: '@next/font/google', options: { subsets: ['latin'] } },
