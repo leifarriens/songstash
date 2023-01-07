f1192acb785d9cbdaaf622afb143fe3d700115e9
Leif Arriens <leif.arriens@gmail.com>
2023-01-07 22:35:19 +0100

    add(mobile): font loader

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -1,15 +1,33 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { httpBatchLink } from '@trpc/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { LinearGradient } from 'expo-linear-gradient';
 import Constants from 'expo-constants';
 import { trpc } from './utils/trpc';
 import { Discover } from './modules/Discover';
 import { View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import * as SplashScreen from 'expo-splash-screen';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_700Bold,
+} from '@expo-google-fonts/inter';
 
 const API_URL = Constants.expoConfig?.extra?.API_URL;
 
 function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_700Bold,
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
   const [queryClient] = useState(
     () =>
       new QueryClient({
@@ -34,10 +52,18 @@ function App() {
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
-        <View className="flex-1 bg-black">
-          <StatusBar style="light" />
-          <Discover />
-        </View>
+        {fontsLoaded && (
+          <View className="flex-1 bg-black" onLayout={onLayoutRootView}>
+            <StatusBar style="light" />
+
+            <Discover />
+
+            <LinearGradient
+              colors={['transparent', 'rgb(0,0,0)']}
+              className="absolute w-full bottom-0 h-16 z-30"
+            />
+          </View>
+        )}
       </QueryClientProvider>
     </trpc.Provider>
   );
