cb764b26121282428640ccc4a3891c7d8605bdea
Leif Arriens <leif.arriens@gmail.com>
2023-01-10 02:45:06 +0100

    add(mobile): debounce filters drilled into Recom.

--- apps/mobile/src/modules/Discover/Discover.tsx
+++ apps/mobile/src/modules/Discover/Discover.tsx
@@ -2,15 +2,18 @@ import { useReducer } from 'react';
 import { View } from 'react-native';
 import { Filter, filterReducer, initialFilterState } from './components/Filter';
 import { Recommendations } from './components/Recommendations';
+import { useDebounce } from 'use-debounce';
 
 export function Discover() {
   const [filters, dispatch] = useReducer(filterReducer, initialFilterState);
+
+  const [debouncedFilters] = useDebounce(filters, 500);
 
   return (
     <View>
       <Filter filters={filters} dispatch={dispatch} />
 
-      <Recommendations filters={filters} />
+      <Recommendations filters={debouncedFilters} />
     </View>
   );
 }
